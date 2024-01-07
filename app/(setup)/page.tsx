15e64c3dbe8b08597d34aeba8ca0bb0dc8c5333b
JSTKode <mehdi.cesa@outlook.fr>
2024-01-07 22:22:01 +0100

    avancement

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -9,19 +9,19 @@ const SetupPage = async () => {
 
   const server = await db.server.findFirst({
     where: {
-        members: {
-            some: {
-                profileId: profile.id
-            }
+      members: {
+        some: {
+          profileId: profile.id
         }
+      }
     }
   });
 
   if (server) {
-    return redirect(`/server/${server.id}`)
+    return redirect(`/servers/${server.id}`);
   }
 
   return <InitialModal />;
-};
-
+}
+ 
 export default SetupPage;
